3a3dea9172aae193036efd462afc004d390b6332
codermehraj <codermehraj@gmail.com>
2023-10-15 09:01:11 +0600

    fixed issues

--- Flutter/hack_lu/lib/main.dart
+++ Flutter/hack_lu/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:hack_lu/apiTest.dart';
 import 'package:hack_lu/pages/dashboard.dart';
 import 'package:hack_lu/pages/form/form_four.dart';
@@ -14,17 +13,7 @@ import 'package:hack_lu/push_noti/firebase_not.dart';
 import 'package:hack_lu/widgets/map_screen.dart';
 import 'package:hack_lu/widgets/profile_cart.dart';
 
-
 import 'firebase_options.dart';
-=======
-import 'package:hack_lu/SchedulePage.dart';
-import 'package:hack_lu/pages/bookingAcceptPage.dart';
-import 'package:hack_lu/pages/dashboard.dart';
-import 'package:hack_lu/pages/myBookings.dart';
-import 'package:hack_lu/pages/profile.dart';
-import 'package:hack_lu/widgets/profile_cart.dart';
-
->>>>>>> f10e085965b5f5c14d39982404eb8d7c54c4574d
 import 'pages/login_page.dart';
 import 'pages/register_page.dart';
 import './pages/bookNow.dart';
@@ -34,13 +23,10 @@ Widget _defaultHome = const LoginPage();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-<<<<<<< HEAD
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   //await FirebaseApi().initNotifications();
-=======
->>>>>>> f10e085965b5f5c14d39982404eb8d7c54c4574d
 
   runApp(const MyApp());
 }
@@ -59,21 +45,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 17, 71, 87)),
       ),
-<<<<<<< HEAD
       home: FormOne(),
-     //  routes: {
-     //    '/': (context) => _defaultHome,
-     //    '/login': (context) => const LoginPage(),
-     //    '/register': (context) => const RegisterPage(),
-     //  },
-=======
-      home: SchedulePage(),
       //  routes: {
       //    '/': (context) => _defaultHome,
       //    '/login': (context) => const LoginPage(),
       //    '/register': (context) => const RegisterPage(),
       //  },
->>>>>>> f10e085965b5f5c14d39982404eb8d7c54c4574d
     );
   }
 }
